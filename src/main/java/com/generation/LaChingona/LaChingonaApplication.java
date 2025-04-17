50546ef54c468b68ddd0f057561394b8e91abc00
User <user@Users-MacBook-Pro.local>
2025-04-17 10:15:27 -0300

    Iniciação do projeto integrador

---
+++ src/main/java/com/generation/LaChingona/LaChingonaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.LaChingona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaChingonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaChingonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/LaChingona/LaChingonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.LaChingona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaChingonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
